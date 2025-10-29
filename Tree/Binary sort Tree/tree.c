c2ad0f2cf46531ebf956f3414747f712ffc220c5
31377 <3137753560@qq.com>
2025-10-29 10:50:12 +0800

    实现创建新节点和插入节点的函数

--- Tree/Binary sort Tree/tree.c
+++ Tree/Binary sort Tree/tree.c
@@ -1,2 +1,28 @@
 #include "tree.h"
 
+// 创建新节点
+Node *creatNode(Node *root, int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+
+    if (newNode == NULL)
+    {
+        printf("分配空间失败\n");
+        exit(1); // 直接中止整个程序
+    }
+
+    // 初始化节点
+    // 数据域
+    newNode->data = data;
+    // 指针域
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// 插入节点
+Node *insert(Node *root, int data)
+{
+    
+}
--- Tree/Binary sort Tree/tree.h
+++ Tree/Binary sort Tree/tree.h
@@ -6,3 +6,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+// 定义二叉树的节点结构
+typedef struct Node
+{
+    int data;
+    struct Node *left;  // 左子树
+    struct Node *right; // 右子树
+} Node;
+
+extern Node *creatNode(Node *root, int data);
+extern Node *insert(Node *root, int data);
